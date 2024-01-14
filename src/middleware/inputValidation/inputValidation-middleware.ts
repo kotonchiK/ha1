02270a5c3210e94893e58e7406e270fc6e1d99f2
kotonchik <anton.mut05@gmail.com>
2024-01-14 22:08:17 +0100

    ha2.updInpValidMw.ts

--- src/middleware/inputValidation/inputValidation-middleware.ts
+++ src/middleware/inputValidation/inputValidation-middleware.ts
@@ -2,11 +2,12 @@ import {NextFunction, Request, Response} from "express";
 import {ValidationError, validationResult} from "express-validator";
 
 export const inputValidationMiddleware = (req:Request, res:Response, next:NextFunction) => {
-    const formattedError = validationResult(req).formatWith((error:ValidationError ) => ({
-        message: error.msg,
-        field: error.type === 'field' ? error.path: 'unknown'
-
-    }))
+    const formattedError = validationResult(req).formatWith((error:ValidationError ) => {
+        return {
+            message: error.msg,
+            field: error.type === 'field' ? error.path : 'unknown'
+        }
+    })
 
     if(!formattedError.isEmpty()) {
         const errorMessage = formattedError.array({onlyFirstError:true})
