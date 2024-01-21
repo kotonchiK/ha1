02a8263ff37a649a025f7b598a2a9f97f5581038
kotonchik <anton.mut05@gmail.com>
2024-01-21 16:36:17 +0100

    ha2.updInpValidMw.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import {runDb} from "./db/db";
-import {app} from "./app";
+import express from "express";
+import {RouterPaths} from "./routerPaths";
+import {blogsRouter} from "./features/blogs/routers/blogs-router";
+import {postsRouter} from "./features/posts/router/posts-router";
+import {testRouter} from "./test/test-router";
 
 export const port= 3000
+export const app = express()
+
 
 app.listen(port, async () => {
 await runDb()
 })
 
+export const jsonBodyMiddleWare = express.json()
+
+app.use(jsonBodyMiddleWare)
+
+app.use(RouterPaths.blogs, blogsRouter)
+app.use(RouterPaths.posts, postsRouter)
+app.use(RouterPaths.testing, testRouter)
